6d0d32b8aa3bf5e109f469fb9135bc20f3190146
Meserret Kabakcı <76451691+meserretkabakci@users.noreply.github.com>
2020-12-21 23:44:47 +0300

    Add files via upload

---
+++ PythonOdev1.py
@@ -0,0 +1,33 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> u_name = input ("Please enter your name:")
+Please enter your name:meserret
+>>> u_lname = input("Please enter your last name:")
+Please enter your last name:kabakci
+>>> u_age = int(input("Please enter your age:"))
+Please enter your age:22
+>>> u_id = int(input("Please enter your id:"))
+Please enter your id:322974
+>>> u_salary = float(input("Please enter your salary:"))
+Please enter your salary:4321.23
+>>> print(f' name: {u_name}')
+ name: meserret
+>>> print("surname:", u_lname)
+surname: kabakci
+>>> print(f' age: {u_age}')
+ age: 22
+>>> print("id:", u_id)
+id: 322974
+>>> print(f' salary: {u_salary}')
+ salary: 4321.23
+>>> type(u_name)
+<class 'str'>
+>>> type(u_lname)
+<class 'str'>
+>>> type(u_age)
+<class 'int'>
+>>> type(u_id)
+<class 'int'>
+>>> type(u_salary)
+<class 'float'>
+>>> 
